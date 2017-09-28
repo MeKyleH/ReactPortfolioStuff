837e1e94c03be29c495441acac3b5d87b12e8548
kholt <kyle.holt@sirsidynix.com>
2017-09-27 22:23:58 -0600

    creating basic search bar

--- src/index.js
+++ src/index.js
@@ -1,45 +1,16 @@
 import React, { Component }from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/youtube/search_bar';
-import VideoList from './components/youtube/video_list';
-import YTSearch from 'youtube-api-search';
 
 const YOUTUBE_API_KEY = 'AIzaSyBDURW3R1T9q2cWT1tTIaP8dNx8ivryPxU';
 
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { videos: {} };
-
-    YTSearch({key: YOUTUBE_API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos });
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <SearchBar />
-        <VideoList videos={this.state.videos} />
-      </div>
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
     );
-  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
-
-//const GMAIL_API_KEY = '640149538778-6qpkv6hr7fld762i34rprcq81gc4sb07.apps.googleusercontent.com';
-
-/*
- ----- THIS IS FROM THE GOOGLE EXAMPLE -----
- ----- https://developers.google.com/maps/documentation/javascript/earthquakes -----
-
- // Create a script tag and set the USGS URL as the source.
- var script = document.createElement('script');
-
- script.src = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp';
- document.getElementsByTagName('head')[0].appendChild(script);
-
- */
