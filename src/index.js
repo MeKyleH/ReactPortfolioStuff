66de62d34491930d55c151fc5cc23fec5dab6abe
kholt <kyle.holt@sirsidynix.com>
2017-09-27 23:32:25 -0600

    performing searches when typing in search bar

--- src/index.js
+++ src/index.js
@@ -16,7 +16,11 @@ class App extends Component {
       selectedVideo: null
     };
 
-    YTSearch({key: YOUTUBE_API_KEY, term: 'digitalslomads'}, (videos) => {
+    this.videoSearch('digitalslomads');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: YOUTUBE_API_KEY, term: term}, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
@@ -27,7 +31,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
