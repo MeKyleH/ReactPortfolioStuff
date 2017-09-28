fe2cedcf1d24b130eb7a9e718b7dfb045fa60f78
kholt <kyle.holt@sirsidynix.com>
2017-09-27 22:37:36 -0600

    testing youtube api searching

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React, { Component }from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/youtube/search_bar';
+import YTSearch from 'youtube-api-search';
 
 const YOUTUBE_API_KEY = 'AIzaSyBDURW3R1T9q2cWT1tTIaP8dNx8ivryPxU';
 
+YTSearch({key: YOUTUBE_API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
 
 const App = () => {
   return (
