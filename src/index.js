a7e7c1ad22c848a92c998b3c8e72623e916b01c6
kholt <kyle.holt@sirsidynix.com>
2017-09-27 22:47:39 -0600

    started video list component

--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
-import React, { Component }from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/youtube/search_bar';
+import VideoList from './components/youtube/video_list';
 import YTSearch from 'youtube-api-search';
 
 const YOUTUBE_API_KEY = 'AIzaSyBDURW3R1T9q2cWT1tTIaP8dNx8ivryPxU';
 
-YTSearch({key: YOUTUBE_API_KEY, term: 'surfboards'}, function(data) {
-  console.log(data);
-});
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
+    this.state = { videos: [] };
+
+    YTSearch({key: YOUTUBE_API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos}/>
+      </div>
     );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
