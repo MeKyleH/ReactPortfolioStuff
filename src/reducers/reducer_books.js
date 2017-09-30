e01eeff8e5af80f60ca0e71e087d09257d836ef6
kholt <kyle.holt@sirsidynix.com>
2017-09-29 19:33:52 -0600

    wiring up book details and creating initial state message

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,13 +1,13 @@
 export default function() {
   return [
-    { title: 'Harry Potter 1'},
-    { title: 'Harry Potter 2'},
-    { title: 'Harry Potter 3'},
-    { title: 'Harry Potter 4'},
-    { title: 'Harry Potter 5'},
-    { title: 'Harry Potter 6'},
-    { title: 'Harry Potter 7'},
-    { title: 'Harry Potter 8'},
-    { title: 'Fantastic Beasts And Where To Find Them'}
+    { title: 'Harry Potter 1', pages: 101},
+    { title: 'Harry Potter 2', pages: 202},
+    { title: 'Harry Potter 3', pages: 303},
+    { title: 'Harry Potter 4', pages: 404},
+    { title: 'Harry Potter 5', pages: 505},
+    { title: 'Harry Potter 6', pages: 606},
+    { title: 'Harry Potter 7', pages: 707},
+    { title: 'Harry Potter 8', pages: 808},
+    { title: 'Fantastic Beasts And Where To Find Them', pages: 909}
   ];
 }
