7ac4e269cb74a94f147cb07e9717c944abc928a8
kholt <kyle.holt@sirsidynix.com>
2017-09-29 18:57:03 -0600

    adding a few more titles for more content

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -2,6 +2,12 @@ export default function() {
   return [
     {title: 'Harry Potter 1'},
     {title: 'Harry Potter 2'},
-    {title: 'Harry Potter 3'}
+    {title: 'Harry Potter 3'},
+    {title: 'Harry Potter 4'},
+    {title: 'Harry Potter 5'},
+    {title: 'Harry Potter 6'},
+    {title: 'Harry Potter 7'},
+    {title: 'Harry Potter 8'},
+    {title: 'Fantastic Beasts And Where To Find Them'}
   ];
 }
